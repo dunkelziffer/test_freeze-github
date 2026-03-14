6fb14861b0f723eabca7e8d1b2c64003c52a2a52
Klaus Weidinger <weidkl@gmx.de>
2026-03-14 21:08:57 +0100

    bin/rails g system_test signup_and_login

---
+++ test/system/signup_and_logins_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SignupAndLoginsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit signup_and_logins_url
+  #
+  #   assert_selector "h1", text: "SignupAndLogin"
+  # end
+end
